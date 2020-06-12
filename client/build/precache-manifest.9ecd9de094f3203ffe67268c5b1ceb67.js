017b225666bf0ddb41c812656c062331d8b7964a
Gus Cramer <cramerg2@wwu.edu>
2020-06-12 12:58:04 -0700

    added build

---
+++ client/build/precache-manifest.9ecd9de094f3203ffe67268c5b1ceb67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07636606a36341dd8d1712bc77f3ed71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27ef6df9917711080ca0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8160d5daa21ee168c41",
+    "url": "/static/js/2.2ac8f9b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2ac8f9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ef6df9917711080ca0",
+    "url": "/static/js/main.5fc6ab7c.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
